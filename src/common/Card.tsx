bb969b946a3d6f676a212f04a9117a631a7adb3f
Alexandra <alexandralsilva91@gmail.com>
2025-05-07 08:26:40 +0100

    added rating stars to the card

--- src/common/Card.tsx
+++ src/common/Card.tsx
@@ -9,6 +9,32 @@ interface IProps {
     poster: string;
 }
 
+function convertRating(rating: number) {
+    const convertedRatio = Number((rating / 2).toFixed(1));
+
+    if (convertedRatio <= 0.5) {
+        return '½';
+    } else if (convertedRatio <= 1) {
+        return '★';
+    } else if (convertedRatio <= 1.5) {
+        return '★½';
+    } else if (convertedRatio <= 2) {
+        return '★★';
+    } else if (convertedRatio <= 2.5) {
+        return '★★½';
+    } else if (convertedRatio <= 3) {
+        return '★★★';
+    } else if (convertedRatio <= 3.5) {
+        return '★★★½';
+    } else if (convertedRatio <= 4) {
+        return '★★★★';
+    } else if (convertedRatio <= 4.5) {
+        return '★★★★½';
+    } else {
+        return '★★★★★';
+    }
+}
+
 function Card({ id, title, year, rating, poster }: IProps) {
     return (
         <Link to={`/movie/${id}`} style={{ textDecoration: 'none', color: 'white' }}>
@@ -16,7 +42,7 @@ function Card({ id, title, year, rating, poster }: IProps) {
                 <img src={`https://image.tmdb.org/t/p/w500${poster}`} className="card__poster" alt="movie poster" />
                 {title ? <h3 className="card__title" title={title}>{title}</h3> : null}
                 {year ? <div className="card__year">{new Date(year).getFullYear()}</div> : null}
-                <div className="card__rating">{rating}</div>
+                <div className="card__rating">{convertRating(rating)}</div>
             </div>
         </Link>
     );
